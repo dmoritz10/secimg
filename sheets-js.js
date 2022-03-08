1a7a365b7346615c6f75b64cfb354e476771ac4e
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-07 16:36:16 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -369,6 +369,18 @@ async function btnDeleteSheetHtml() {
 
   if (!confirmOK) return
 
+  try {
+  	const clipboardItems = await navigator.clipboard.read();
+    console.log(clipboardItems);
+  	const blobOutput = await clipboardItems[0].getType('image/png');
+    document.getElementById('shtmImgFront').src =
+      window.URL.createObjectURL(blobOutput);
+    log('Image pasted.');
+  } catch(e) {
+  	log('Failed to read clipboard');
+  }
+  return
+
 
   var idx = shtIdxArr[$('#shtmArrIdx').val() * 1]
 
