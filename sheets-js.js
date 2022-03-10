664f57b650a7af86f66c4260248722d3117345dc
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-10 10:17:58 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -457,12 +457,13 @@ function dupDocument(Document) {
 }
 
 
-async function showFile(input) {
+async function showFile(input, type) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
 
     reader.onload = function (e) {
-        $('#shtmImgFront').attr('src', e.target.result);
+      if (type=="front") $('#shtmImgFront').attr('src', e.target.result);
+      else $('#shtmImgBack').attr('src', e.target.result);
 
         
 
