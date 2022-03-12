68b43c53b6e745381f0725e09a980eb4230d97e5
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-12 15:20:39 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -38,11 +38,13 @@ async function showSheet(idx) {
   var val
   var icon
   imgs[0] ? val = '<span><img class="showImg" src=' + imgs[0] + "></img></span>" : val=''
-  icon =  viewImgTemplate.replace("#img#", imgs[0])
+  // icon =  viewImgTemplate.replace("#img#", imgs[0])
+  icon =  viewImgTemplate
   sht.push(['Front', val, icon])
 
   imgs[1] ? val = '<span><img class="showImg" src=' + imgs[1] + "></img></span>" : val=''
-  icon =  viewImgTemplate.replace("#img#", imgs[1])
+  // icon =  viewImgTemplate.replace("#img#", imgs[1])
+  icon =  viewImgTemplate
   sht.push(['Back', val, icon])
   
   var tbl = new Table();
