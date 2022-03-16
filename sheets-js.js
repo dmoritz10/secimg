bd1434dbb9094bba3ad0d084b55114a0b3d17b36
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-16 03:21:21 -0700

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -298,7 +298,7 @@ async function btnShtmSubmitSheetHtml() {
   $('#shtmImgFront').removeAttr('src').addClass('d-none')
   $('#shtmImgBack').removeAttr('src').addClass('d-none')
 
-  $("#sheet-modal").modal('hide');
+  $("#sheet-modal").modal('dispose');
 
   updateUI(valsEnc, arrIdx)
 
@@ -488,7 +488,7 @@ console.log('delete file id', $('#shtmFileId').val())
 });
 
 
-  $("#sheet-modal").modal('hide');
+  $("#sheet-modal").modal('dispose');
 
   listSheet(shtTitle)
 
