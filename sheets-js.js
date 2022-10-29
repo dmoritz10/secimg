7219196e1572db554d0e0d9b175568e2fa7a2cad
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-10-29 14:15:34 -0700

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -1,34 +1,6 @@
 
 async function listSheet(title) {
 
-  if (!enhancer) {
-    alert('2')
-
-    enhancer = await Dynamsoft.DCE.CameraEnhancer.createInstance();
-    alert('3')
-    // await enhancer.setUIElement(Dynamsoft.DCE.CameraEnhancer.defaultUIElementURL);
-    alert('3.1')
-    // enhancer.setResolution(1280, 720)
-    // enhancer.setViewDecorator("focus")
-    alert('4')
-    // console.log('getallcameras', await enhancer.getAllCameras())
-    alert('4.1')
-    try {
-    await enhancer.open();
-    } catch(err) {alert("4.2");console.log('err', err)}
-    alert('5')
-    
-    const d = enhancer.getUIElement()
-    $( d ).css( {position: 'relative'} );
-    document.getElementById("enhancerUIContainer").appendChild(d)
-    alert('6')
-    
-    $(".dce-btn-close").addClass('d-none')
-    $(".dce-msg-poweredby").addClass('d-none')
-    $("#cameraOverlay").removeClass('d-none')
-
-  }
-
   modal(true)
 
   var shtOptions = readOption('shtFilter')
@@ -748,46 +720,32 @@ async function clearImage(shtTitle, row) {        // recall that the sheet title
 
 async function startCamera(frntBack) {
 
-  alert('1')
-
   if (!enhancer) {
-    alert('2')
 
     enhancer = await Dynamsoft.DCE.CameraEnhancer.createInstance();
-    alert('3')
     // await enhancer.setUIElement(Dynamsoft.DCE.CameraEnhancer.defaultUIElementURL);
-    alert('3.1')
     // enhancer.setResolution(1280, 720)
     // enhancer.setViewDecorator("focus")
-    alert('4')
     // console.log('getallcameras', await enhancer.getAllCameras())
-    alert('4.1')
-    try {
     await enhancer.open();
-    } catch(err) {alert("4.2");console.log('err', err)}
-    alert('5')
     
     const d = enhancer.getUIElement()
     $( d ).css( {position: 'relative'} );
     document.getElementById("enhancerUIContainer").appendChild(d)
-    alert('6')
     
     $(".dce-btn-close").addClass('d-none')
     $(".dce-msg-poweredby").addClass('d-none')
 
   }
-  alert('7')
 
   document.getElementById("enhancerUIContainer").dataset.frntback = frntBack;
-  alert('8')
   
   if (frntBack == 'front')  $('#shtmImgFront').addClass('d-none');
   else                      $('#shtmImgBack').addClass('d-none');
-  alert('9')
 
   $("#cameraOverlay").removeClass('d-none')
-  alert('10')
-
+  
+  frntBack.preventDefault();
 }
 
 function clickPhoto() {
