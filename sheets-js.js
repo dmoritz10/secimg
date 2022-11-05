04718e696aa8b777ae37f1e2a254b7aaf616bc49
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-11-05 09:45:06 -0700

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -1203,7 +1203,7 @@ ctx.drawImage(img,sx,sy,sw,sh,x,y,w,h)
     console.log('move', mousePos1)
   
     if (isDragging1 == true) {
-      ctx1.clearRect(1, 1, cw-2, ch-2);
+      ctx1.clearRect(0, 0, canvas.width, canvas.height);
   console.log('cleaerRect', cw, ch)
       for (k in o) {
         if (o[k].bool) {
