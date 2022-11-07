ce912841e161f2c822787db80d4e3293132db2e2
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-11-07 07:48:19 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -1161,7 +1161,7 @@ ctx.drawImage(img,sx,sy,sw,sh,x,y,w,h)
   drawGuides(o);
   var imgo = Imgo(o, d); // an object defining the cropped image
   c1.style.backgroundImage = 'url(' + img.src + ')';
-  c1.style.backgroundSize =  'auto auto'
+  c1.style.backgroundSize =  'cover'
   drawCroppedImage(imgo);
   
   // mousedown ***************************
