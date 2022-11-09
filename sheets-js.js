5bf099e47c89e709b0be48a60c95cf7cee3b6ed5
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-11-09 08:04:49 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -1100,7 +1100,12 @@ ctx.drawImage(img,sx,sy,sw,sh,x,y,w,h)
   
   function drawCroppedImage(imgo) {
     // ctx2.drawImage(img, imgo.sx, imgo.sy, imgo.sw, imgo.sh, imgo.x, imgo.y, imgo.w, imgo.h);
-    ctx2.drawImage(img, imgo.sx, imgo.sy, imgo.sw, imgo.sh, 0, 0, imgo.w, imgo.h);
+    // ctx2.drawImage(img, imgo.sx, imgo.sy, imgo.sw, imgo.sh, 0, 0, imgo.w, imgo.h);
+
+    var w = canvas.width
+    var h = canvas.height*imgo.w/imgo.h
+
+    ctx2.drawImage(img, imgo.sx, imgo.sy, imgo.sw, imgo.sh, 0, 0, w, h);
     Output(Imgo, output); 
   }
   
