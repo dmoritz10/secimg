ee2563c0b12f8297afebe00d468b7680ed76d23b
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-11-13 05:38:29 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -1005,6 +1005,7 @@ async function setupCrop(c, imgSrc) {
     dogImg.src = imgSrc;
     dogImg.onload = function () {
       var oImg = new fabric.Image(dogImg);
+      console.log('oImg1', oImg.width, oImg.height)
 
       let imgWidth = oImg.width;
       let imgHeight = oImg.height;
@@ -1023,9 +1024,9 @@ async function setupCrop(c, imgSrc) {
           oImg.scaleToWidth(canvasWidth);
           console.log('scaleToWidth', canvasWidth)
         }
-        console.log('oImg', oImg.width, oImg.width)
+        console.log('oImg2', oImg.width, oImg.height)
     
-        console.log('hw', canvasHeight, canvasWidth)
+        console.log('canvas', canvasWidth, canvasHeight)
       canvas.add(oImg);
       canvas.centerObject(oImg);
       canvas.setActiveObject(oImg);
