692f094b42362a19ca91513e6e0102cf7ef7d31c
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-11-15 06:45:38 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -1173,7 +1173,7 @@ async function setupCrop(frntback) {
       
       console.log($('#shtmFront'))
       $(fb.canvas).parent('.canvas-container').remove();
-      $('<canvas id="shtmCanvasFront" class="d-none"></canvas>').appendTo($('#shtmFront'));
+      // $('<canvas id="shtmCanvasFront" class="d-none"></canvas>').appendTo($('#shtmFront'));
       console.log($('#shtmFront'))
 
     });
