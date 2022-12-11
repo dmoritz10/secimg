1d40a3ce67855da278c088499711959236a09af4
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-12-11 11:50:33 -0800

    dan

--- sheets-pdf.js
+++ sheets-pdf.js
@@ -34,7 +34,7 @@ _PAGE_RENDERING_IN_PROGRESS = 1;
 _CURRENT_PAGE = page_no;
 
 // update current page
-document.getElementById(pdfPagesFront).innerHTML = page_no + '/' + _TOTAL_PAGES;
+document.getElementById('pdfPagesFront').innerHTML = page_no + '/' + _TOTAL_PAGES;
 
 // get handle of page
 try {
