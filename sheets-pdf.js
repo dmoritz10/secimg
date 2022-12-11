0bee1deb608b188a8fe481be432d537f699c1962
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-12-11 10:21:54 -0800

    dan

--- sheets-pdf.js
+++ sheets-pdf.js
@@ -34,7 +34,7 @@ _PAGE_RENDERING_IN_PROGRESS = 1;
 _CURRENT_PAGE = page_no;
 
 // update current page
-document.querySelector("_fb.options.pagesPDF").innerHTML = page_no + '/' + _TOTAL_PAGES;
+document.getElementById(_fb.options.pagesPDF).innerHTML = page_no + '/' + _TOTAL_PAGES;
 
 // get handle of page
 try {
@@ -85,13 +85,13 @@ _PAGE_RENDERING_IN_PROGRESS = 0;
 }
 
 // click on the "Previous" page button
-document.querySelector("#pdf-prev").addEventListener('click', function() {
+document.getElementById(_fb.options.prevPagePDF).addEventListener('click', function() {
 if(_CURRENT_PAGE != 1)
     showPage(--_CURRENT_PAGE);
 });
 
 // click on the "Next" page button
-document.querySelector("#pdf-next").addEventListener('click', function() {
+document.getElementById(_fb.options.nextPagePDF).addEventListener('click', function() {
 if(_CURRENT_PAGE != _TOTAL_PAGES)
     showPage(++_CURRENT_PAGE);
 });
