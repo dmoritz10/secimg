8261b2f85a259432a6477e14566107bf355ab688
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-12-13 16:27:00 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -151,7 +151,7 @@ function openImg(img) {
 
 function openPDF(img) {
 
-  var win = window.open("", "Title", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=480,top="+(screen.height-200)+",left="+(screen.width-400));
+  var win = window.open("", "Title", "toolbar=no,location=yes,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=480,top="+(screen.height-200)+",left="+(screen.width-400));
   win.document.body.innerHTML = '<iframe width="100%" height="100%" src="https://docs.google.com/gview?url="' + img + '></iframe>';
 
 }
