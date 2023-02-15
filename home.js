c3dde35d244051155d01b6ef6fc1cdd63dcfefa8
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-02-15 08:06:08 -0800

    dan

--- home.js
+++ home.js
@@ -1,14 +1,9 @@
 
 async function loadSheets() {
 
-  console.log('loadSheets begin')
-
     var shts = await getSheets()
 
     var sheets = shts.result.sheets
-    
-    console.log('loadSheets', sheets, sheets == true)
-
 
     if (sheets) {
 
@@ -21,8 +16,6 @@ async function loadSheets() {
       var nbrSheets = 0
       var nbrDocuments = 0
 
-      console.log('sheets.length', sheets.length)
-    
       for (var j = 0; j < sheets.length; j++) {
 
         var sht = sheets[j].properties
@@ -47,9 +40,6 @@ async function loadSheets() {
 
           secSht[sht.title].enc = enc.enc
           secSht[sht.title].isSecSht = enc.isSecSht
-
-          console.log('enc', enc)
-
 
           if (enc.isSecSht) {
 
