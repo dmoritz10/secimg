00ed29fab30ad378bec84812279d36835a0aee2e
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-02-15 08:23:13 -0800

    dan

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 
 const API_KEY   = 'AIzaSyA47A-jDhu3jOQZqo6OYOV4aGsCIY0YT4M'
 const CLI_ID    = '806474795673-vcajgv4ncv6rq09j4o01d8tg7f487smm.apps.googleusercontent.com'  
-const SCOPES    = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.metadata.readonly'; // Space delimited if more than one
+const SCOPES    = 'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive';
 const DISCOVERY = ['https://sheets.googleapis.com/$discovery/rest?version=v4', 
                   'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
                   ];
